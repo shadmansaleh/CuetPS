9b3497469cd229d2ab1f64c33f6443fed5c77c5e
Adiba <adibafairoozchowdhury@gmail.com>
2025-02-06 12:41:01 +0600

    exhibition searching

--- frontend/src/pages/Admin/ExhibitionTab.tsx
+++ frontend/src/pages/Admin/ExhibitionTab.tsx
@@ -1,5 +1,4 @@
-// AdminPage.tsx
-import { Button, Modal } from "antd";
+import { Button, Modal, Input } from "antd";
 import { useState } from "react";
 import CreateExhibition from "./CreateExhibition";
 import ExhibitionTable from "./ExhibitionTable";
@@ -8,8 +7,11 @@ import axios from "@/utils/axios";
 import Loading from "@/components/Loading";
 import { Exhibition } from "@/types";
 
+const { Search } = Input;
+
 function ExhibitionTab() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const [exhibitions, setExhibitions] = useState<Exhibition[]>([]);
   const exhibitionQuery = useQuery(
@@ -33,16 +35,31 @@ function ExhibitionTab() {
     setIsModalVisible(false);
   };
 
+  // Filter exhibitions based on search query
+  const filteredExhibitions = exhibitions.filter((exhibition) =>
+    exhibition.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   if (exhibitionQuery.isLoading) return <Loading />;
+  
   return (
     <div className="flex flex-col">
-      <Button
-        type="primary"
-        onClick={handleCreateExhibitionClick}
-        className="mb-5 ml-auto "
-      >
-        Create Exhibition
-      </Button>
+      <div className="flex justify-between mb-5">
+        <Search
+          placeholder="Search exhibition..."
+          allowClear
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-1/2"
+        />
+        <Button
+          type="primary"
+          onClick={handleCreateExhibitionClick}
+          className="ml-auto"
+        >
+          Create Exhibition
+        </Button>
+      </div>
+
       {/* Modal for CreateExhibition */}
       <Modal
         title="Create Exhibition"
@@ -54,10 +71,16 @@ function ExhibitionTab() {
       >
         <CreateExhibition />
       </Modal>
+
       {exhibitionQuery.isError && <div>Failed to load exhibitions</div>}
       {exhibitionQuery.isLoading && <Loading />}
+
+      {!exhibitionQuery.isLoading && filteredExhibitions.length === 0 && (
+        <div>No exhibitions found.</div>
+      )}
+
       {!exhibitionQuery.isLoading &&
-        exhibitions.map((exhibition) => (
+        filteredExhibitions.map((exhibition) => (
           <ExhibitionTable
             key={exhibition._id}
             exhibitionId={exhibition._id}
